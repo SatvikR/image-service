c978e1b16f0d89fd59a14f3cc75998726b46caab
Satvik Reddy <reddy.satvik@gmail.com>
2021-06-01 11:47:54 -0700

    add delete route

--- server.go
+++ server.go
@@ -6,25 +6,36 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"errors"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"mime/multipart"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 )
 
+//
+// Configuration and Setup
+//
 type Config struct {
 	AccessKey    string
 	SecretKey    string
 	S3bucketName string
 	Region       string
+	KeyPrefix    string
+}
+
+type DeleteReq struct {
+	URL string `json:"url"`
 }
 
 var ValidFiles = map[string]string{
@@ -35,16 +46,35 @@ var ValidFiles = map[string]string{
 }
 
 var uploader *s3manager.Uploader
+var deleter *s3manager.BatchDelete
 var config Config
 
 func loadConfig() Config {
+	var KeyPrefix strings.Builder
+
+	AccessKey := os.Getenv("AWS_ACCESS_KEY")
+	SecretKey := os.Getenv("AWS_SECRET_KEY")
+	S3bucketName := os.Getenv("AWS_S3_BUCKET_NAME")
+	Region := os.Getenv("AWS_REGION")
+
+	KeyPrefix.WriteString("https://")
+	KeyPrefix.WriteString(S3bucketName)
+	KeyPrefix.WriteString(".s3.")
+	KeyPrefix.WriteString(Region)
+	KeyPrefix.WriteString(".amazonaws.com/")
+
 	return Config{
-		os.Getenv("AWS_ACCESS_KEY"),
-		os.Getenv("AWS_SECRET_KEY"),
-		os.Getenv("AWS_S3_BUCKET_NAME"),
-		os.Getenv("AWS_REGION"),
+		AccessKey,
+		SecretKey,
+		S3bucketName,
+		Region,
+		KeyPrefix.String(),
 	}
 }
+
+//
+// Utility functions used in our routes
+//
 
 func genFileName() (string, error) {
 	buff := make([]byte, 32)
@@ -65,6 +95,10 @@ func getFileType(r io.Reader) (string, error) {
 	ftype := http.DetectContentType(buff)
 
 	return ftype, nil
+}
+
+func validateObjectURL(URL string) bool {
+	return strings.HasPrefix(URL, config.KeyPrefix)
 }
 
 func UploadFromFileHeader(header *multipart.FileHeader) (string, error) {
@@ -94,15 +128,15 @@ func UploadFromFileHeader(header *multipart.FileHeader) (string, error) {
 		return "", errors.New("not a valid file")
 	}
 
-	var filePathBuff bytes.Buffer
+	var fileKey strings.Builder
 
-	filePathBuff.WriteString("images/")
-	filePathBuff.WriteString(filename)
-	filePathBuff.WriteString(ext)
+	fileKey.WriteString("images/")
+	fileKey.WriteString(filename)
+	fileKey.WriteString(ext)
 
 	result, err := uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(config.S3bucketName),
-		Key:    aws.String(filePathBuff.String()),
+		Key:    aws.String(fileKey.String()),
 		Body:   bytes.NewReader(fileBuf.Bytes()),
 	})
 
@@ -111,6 +145,59 @@ func UploadFromFileHeader(header *multipart.FileHeader) (string, error) {
 	}
 
 	return aws.StringValue(&result.Location), nil
+}
+
+func DeleteObjectFromKey(key string) error {
+	objects := []s3manager.BatchDeleteObject{
+		{
+			Object: &s3.DeleteObjectInput{
+				Key:    aws.String(key),
+				Bucket: aws.String(config.S3bucketName),
+			},
+		},
+	}
+
+	err := deleter.Delete(aws.BackgroundContext(), &s3manager.DeleteObjectsIterator{
+		Objects: objects,
+	})
+
+	return err
+}
+
+//
+// Routes
+//
+
+func RDelete(c *gin.Context) {
+	var reqBody DeleteReq
+	if err := c.ShouldBindJSON(&reqBody); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid request body",
+		})
+		return
+	}
+
+	if !validateObjectURL(reqBody.URL) {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid url",
+		})
+		return
+	}
+
+	key := reqBody.URL[len(config.KeyPrefix):]
+
+	err := DeleteObjectFromKey(key)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "unable to delete object",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("%s deleted successfully", reqBody.URL),
+	})
 }
 
 func RUpload(c *gin.Context) {
@@ -125,7 +212,7 @@ func RUpload(c *gin.Context) {
 	URL, err := UploadFromFileHeader(file)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "file couldn't be uploaded",
+			"error": err.Error(),
 		})
 		return
 	}
@@ -140,6 +227,7 @@ func main() {
 	r.MaxMultipartMemory = 5 << 20
 
 	r.POST("/upload", RUpload)
+	r.DELETE("/delete", RDelete)
 
 	config = loadConfig()
 
@@ -153,6 +241,7 @@ func main() {
 	}))
 
 	uploader = s3manager.NewUploader(sess)
+	deleter = s3manager.NewBatchDelete(sess)
 
 	r.Run(":8000")
 }
